4d16451a368db3f4d1d2cda76807fdd388bb7e5b
ScissorEX <maxencedinel@gmail.com>
2025-09-12 20:18:07 -0400

    11-09-25

---
+++ resources/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>chimprank</div>
+
+    <span>
+        <p>Products</p>
+
+        <p>Solutions</p>
+
+        <p>Resources</p>
+
+        <p>Pricing</p>
+    </span>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,23 @@
-import welcomepage from '../resources/vue/welcomepage.vue';
+import Home from '../resources/views/Home.vue';
+import Products from '../resources/views/Products.vue';
+import Solutions from '../resources/views/Solutions.vue';
+import Resources from '../resources/views/Resources.vue';
+import Pricing from '../resources/views/Pricing.vue';
+import Signup from '../resources/views/Signup.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-
-        {path: '/', name: 'Home', component: welcomepage}
-    
-    ]
-
-
+    { path: '/', name: 'Home', component: Home },
+    { path: '/products', name: 'Products', component: Products },
+    { path: '/solutions', name: 'Solutions', component: Solutions },
+    { path: '/resources', name: 'Resources', component: Resources },
+    { path: '/pricing', name: 'Pricing', component: Pricing },
+    { path: '/signup', name: 'Signup', component: Signup },
+];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
 
 export default router;
