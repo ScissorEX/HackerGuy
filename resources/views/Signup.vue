613bcd347eedeb4b20fcd25d9b57236baf59d959
ScissorEX <maxencedinel@gmail.com>
2025-10-12 01:00:02 -0400

    12-10-25

--- resources/views/Signup.vue
+++ resources/views/Signup.vue
@@ -1,15 +1,108 @@
+<script>
+export default {
+    data(){
+        return{
+        formdata : {
+                    name:"",
+                    email: "",
+                    password:"",
+                    password_confirmation:""
+            }
+        }
+    },
+    methods: {
+        async formsubmit() {
+            const token = document.querySelector('meta[name="csrf"]').content;
+            
+            const res = await fetch('api/signup', {
+                method: 'post',
+                headers: {
+                    'X-CSRF-TOKEN': token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.formdata),
+            });
+            const data = await res.json();
+            console.log(data);
+            
+        },
+    },
+};
+
+
+</script>
+
 <template>
     <div>
-        <div>logo</div>
-        <div>enter the whatever</div>
-        <form method="post" action="/signup" @submit.prevent>
-            <label>hello</label>
-            <input type="text" />
-            <input type="submit" />
+        <div id="div60">
+        <div id="logo">
+            <img src="../components/images/monkey.svg" style="width: 100%" />
+        </div>
+        <form @submit.prevent="formsubmit()" id="formdiv">
+            <div id="formin">
+                <label>type your name
+                    <input type="text" name="name" v-model="formdata.name"/>
+                </label>
+            </div>
+               
+            <div id="formin">
+                <label>type your email adress
+                    <input type="text" name="email" v-model="formdata.email"/>
+                </label>
+                
+            </div>
+
+            <div id="formin">
+                <label>type your password
+                    <input type="password" name="password" v-model="formdata.password"/>
+                </label>
+                
+            </div>
+
+            <div id="formin">
+                <label>confirm your password
+                    <input type="password" name="password_confirmation" v-model="formdata.password_confirmation"/>
+                </label>
+                
+            </div>
+
+            <div>
+                <input type="submit" />
+            </div>
+                
+
         </form>
+        </div>
     </div>
 </template>
 
-<script setup></script>
-
-<style scoped></style>
+<style scoped>
+#div60{
+     border: solid #408040;
+     border-width: 0px 3px;
+    box-shadow: 10px 0px 40px 10px rgba(83, 227, 147, 0.25),-10px 0px 40px 10px rgba(83, 227, 147, 0.25);
+        width: max(60vw,500px);
+            justify-self: center;
+            height: 100vh;
+}
+#logo {
+    justify-self: center;
+    width: 200px;
+    padding: 50px;
+}
+#formdiv {
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+}
+#formin{
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    max-width: 300px;
+    gap: 8px;
+}
+</style>
